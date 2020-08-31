24d08461f7052089a68fd1b625317046ee837f2b
Zackary Cammack <zack.cammack@gmail.com>
2020-08-31 17:57:59 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,9 +14,11 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text_array = []
-  text_array = text.scan(/^\w{5}\s/)
-  text_array
+  new_text_array = []
+  original_text = text.split(" ")
+  original_text.each do |word|
+    if word.match(/^\w{5}\s/)
+      
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
