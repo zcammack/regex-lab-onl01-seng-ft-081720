5f44803bbaaaa8b84a2d3e566281ad02bd113efa
Zackary Cammack <zack.cammack@gmail.com>
2020-08-31 17:53:45 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,14 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\A+un/i)
+  new_text_array = []
+  original_string = text.split(" ")
+  original_string.each do |word|
+    if word.match(/(^un)|(ing$)/)
+      new_text_array.push(word)
+    end
+  end
+  return new_text_array
 end
 
 def words_five_letters_long(text)
